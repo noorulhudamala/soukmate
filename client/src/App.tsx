153d02e6c176f0c3b1e5111b89935a1e6c875870
noorulhudamala <noorulhuda.mala@gmail.com>
2023-11-09 07:41:56 +0500

    App routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,15 @@ import ProductPage from "./components/Home";
 import Header from './components/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/Footer";
+import AppRoutes from "./Routes";
 
 function App() {
 
   return (
     <div className="App">
-      <Header />
-      <ProductPage />
-      <Footer />
+      
+      <AppRoutes/>
+     
     </div>
   );
 }
