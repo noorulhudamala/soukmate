19e9d621162eecd1bd52dca156f8fd9cd23865e0
noorulhudamala <noorulhuda.mala@gmail.com>
2023-11-10 06:50:15 +0500

    adding icons to Header component

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -5,20 +5,29 @@ import "./Header.scss";
 import logo from "../../assets/images/logo.png";
 import SearchInput from "../SearchInput";
 import { useNavigate } from "react-router-dom";
-
+import account from "../../assets/images/account.svg";
+import heartOutline from "../../assets/images/heart_outline.svg";
+import Cart from "../Cart";
 const Header: React.FC = () => {
   const navigate = useNavigate();
   return (
     <header className="sticky-header">
-      <div className="logo" onClick={()=> navigate("/")}>
+      <div className="logo" onClick={() => navigate("/")}>
         <img src={logo} alt="shoemate" />
       </div>
       <div className="d-flex">
         <SearchInput />
-        <nav>
+        <nav className="d-flex align-items-center">
           <ul>
-            <li>Account</li>
-            <li>Cart</li>
+            <li className="d-flex">
+              <img className="px-2" src={account} alt="account-icon" />
+            </li>
+            <li className="d-flex">
+              <img className="px-2" src={heartOutline} alt="account-icon" />
+            </li>
+            <li>
+              <Cart fill="#6A35B8"/>
+            </li>
           </ul>
         </nav>
       </div>
